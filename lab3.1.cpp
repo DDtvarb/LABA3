061bb3e507754c7779705201045b85a78650b008
DDtvarb <153864225+DDtvarb@users.noreply.github.com>
2024-03-23 14:01:04 +0300

    Update lab3.1.cpp

--- lab3.1.cpp
+++ lab3.1.cpp
@@ -1,12 +1,10 @@
 ﻿#include <windows.h>
 #include <iostream>
-#include <stdlib.h>
 #include <string>
-#include <cmath>
 using namespace std;
 
 
-int countVowels(const string& str) {
+int F(const string& str) {
 	int count = 0;
 	string vowels = "Аа";
 	for (int i = 0; i < str.length(); i++) {
@@ -22,5 +20,5 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	string tet;
 	cout << "Введите текст: "; getline(cin, tet);
-	cout << "Количество гласных в тексте " << tet << " : " << countVowels(tet) << endl;
+	cout << "Количество гласных в тексте " << tet << " : " << F(tet) << endl;
 }
